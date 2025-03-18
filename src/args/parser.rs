c1d0a6e67703a5664d1fc341c218b0bf772082bc
Rocky Li <aperocky@gmail.com>
2025-03-18 14:11:11 -0400

    Restructure list command to list task and records separately, add closing code 0 as default

--- src/args/parser.rs
+++ src/args/parser.rs
@@ -14,16 +14,20 @@ pub enum Action {
     Task(TaskCommand),
     /// add record
     Record(RecordCommand),
-    /// list tasks or records
-    List(ListCommand),
     /// Finish task or remove records
     Done(DoneCommand),
+    /// list tasks or records
+    #[command(subcommand)]
+    List(ListCommand),
 }
 
 #[derive(Debug, Args)]
 pub struct TaskCommand {
     /// Description of the task
     pub content: String,
+    /// Category of the task
+    #[arg(short, long)]
+    pub category: Option<String>,
     /// Time the task is due, default to EOD
     #[arg(value_parser = validate_timestr)]
     pub timestr: Option<String>,
@@ -33,18 +37,12 @@ pub struct TaskCommand {
 pub struct RecordCommand {
     /// Content of the record
     pub content: String,
-    /// Category of the record (optional, default to record table)
+    /// Category of the record
     #[arg(short, long)]
     pub category: Option<String>,
     /// Time the record should be made, default to current
     #[arg(short = 't', long = "time", value_parser = validate_timestr)]
     pub timestr: Option<String>,
-}
-
-#[derive(Debug, Args)]
-pub struct ListCommand {
-    /// Category to list, e.g. [task, record, record.category]
-    pub category: Option<String>,
 }
 
 #[derive(Debug, Args)]
@@ -56,6 +54,33 @@ pub struct DoneCommand {
     pub closing_code: Option<u8>,
 }
 
+#[derive(Debug, Subcommand)]
+pub enum ListCommand {
+    /// List tasks
+    Task(ListTaskCommand),
+    /// List records
+    Record(ListRecordCommand),
+}
+
+#[derive(Debug, Args)]
+pub struct ListTaskCommand {
+    /// Target completion date - e.g. "today"", only list task marked to be completed today.
+    pub timestr: Option<String>,
+    /// Category of the task
+    #[arg(short, long)]
+    pub category: Option<String>,
+}
+
+#[derive(Debug, Args)]
+pub struct ListRecordCommand {
+    /// Category of the record
+    #[arg(short, long)]
+    pub category: Option<String>,
+    /// days of records to retrieve - e.g. 1 shows record made in the last 24 hours
+    #[arg(short, long)]
+    pub days: Option<u32>
+}
+
 fn validate_timestr(s: &str) -> Result<String, String> {
     match parse_flexible_timestr(s) {
         Ok(_) => Ok(s.to_string()),
@@ -65,10 +90,11 @@ fn validate_timestr(s: &str) -> Result<String, String> {
 
 fn parse_closing_code(s: &str) -> Result<u8, String> {
     match s.to_lowercase().as_str() {
-        "complete" | "done" | "completed" => Ok(0),
-        "cancelled" | "canceled" | "cancel" => Ok(1),
-        "duplicate" => Ok(2),
-        "removed" | "remove" => Ok(3),
+        "ongoing" => Ok(0), // This is default.
+        "complete" | "done" | "completed" => Ok(1),
+        "cancelled" | "canceled" | "cancel" => Ok(2),
+        "duplicate" => Ok(3),
+        "removed" | "remove" => Ok(4),
         _ => {
             s.parse::<u8>().map_err(|_| 
                 format!("Invalid closing code: '{}'. Expected 'completed', 'cancelled', 'duplicate' or a number from 0-255", s)
